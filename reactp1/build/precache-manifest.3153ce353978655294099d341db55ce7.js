be619a9549716e96743f8701e5e3be3847daa03d
“karimshakir” <karimshakir99@gmail.com>
2020-03-04 18:55:58 -0600

    serve -s build

---
+++ reactp1/build/precache-manifest.3153ce353978655294099d341db55ce7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7269f702bb32805c3b2e3e0d8f0dfc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "226745449e5721eb04a4",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "be0fba8b8bdab0316525",
+    "url": "/static/js/2.e98d6495.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.e98d6495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "226745449e5721eb04a4",
+    "url": "/static/js/main.241e9361.chunk.js"
+  },
+  {
+    "revision": "70c42f02af229ee44005",
+    "url": "/static/js/runtime-main.a34f5d6c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
